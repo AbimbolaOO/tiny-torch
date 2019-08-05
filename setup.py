5b025c243832c6d696d1c1bab72b548ab82f50cf
Your Name <abimbolaolayemiwhyte@gmail.com>
2019-08-05 14:05:47 +0100

    setip.py and LICENSE was added

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+
+
+setuptools.setup(
+    name="tinytorch",
+    version="0.0.1",
+    author="Abimbola Olayemi",
+    author_email="abimbolaolayemiwhyte@gmail.com",
+    description="Deep learning library for introducing children to AI",
+    url="https://github.com/AbimbolaOO/tiny-torch",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
